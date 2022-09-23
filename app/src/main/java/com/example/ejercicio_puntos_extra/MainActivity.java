7fddaa01a922464e0905745818bbb312cc1cc8cb
AlexCabrera123 <100866047+AlexCabrera123@users.noreply.github.com>
2022-09-23 10:15:24 -0500

    Final_practica

--- app/src/main/java/com/example/ejercicio_puntos_extra/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_puntos_extra/MainActivity.java
@@ -34,44 +34,47 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<Float> adapter = new ArrayAdapter<Float>(getApplicationContext(), android.R.layout.simple_list_item_1,alnumeros);
 
+        bOrdenar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alnumeros.clear();
+                adapter.notifyDataSetChanged();
+                lvLista.setAdapter(adapter);
+            }
+        });
+
         bGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String input = etGuardar.getText().toString().trim();
-                float aux = Float.parseFloat(input);
+
                 float suma=0;
                 int cantidad=0;
                 float promedio=0;
-                if (input.equals(""))
-                    Toast.makeText(getApplicationContext(),"Favor de ingresar un número",Toast.LENGTH_LONG).show();
-                else{
+
+                if (!input.equals("")) {
+                    float aux = Float.parseFloat(input);
                     alnumeros.add((float) aux);
                     etGuardar.setText("");
                     adapter.notifyDataSetChanged();
                     lvLista.setAdapter(adapter);
                     for (int i = 0; i < alnumeros.size(); i++) {
                         suma = suma + alnumeros.get(i);
-                        cantidad=cantidad+1;
-                        promedio=suma/cantidad;
+                        cantidad = cantidad + 1;
+                        promedio = suma / cantidad;
+                    }
+                    System.out.println("La cantidad de sueldos es " +cantidad);
+                    System.out.println("La suma de los sueldos es "+suma);
+                    System.out.println("El promedio de los sueldos es " +promedio);
+                }else{
+                        Toast.makeText(getApplicationContext(),"Favor de ingresar un número",Toast.LENGTH_LONG).show();
                     }
                     }
-
-                System.out.println("La cantidad de sueldos es " +cantidad);
-                System.out.println("La suma de los sueldos es "+suma);
-                System.out.println("El promedio de los sueldos es " +promedio);
-
-
-                }
 
 
         });
 
-        bOrdenar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                alnumeros.clear();
-            }
-        });
+
 
         }
 
