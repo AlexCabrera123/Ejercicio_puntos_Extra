dee5c2f07283f2fda1a1dd4e927724f8c2a63dce
AlexCabrera123 <100866047+AlexCabrera123@users.noreply.github.com>
2022-09-23 09:57:06 -0500

    practica

--- app/src/main/java/com/example/ejercicio_puntos_extra/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_puntos_extra/MainActivity.java
@@ -30,36 +30,35 @@ public class MainActivity extends AppCompatActivity {
         bOrdenar = findViewById(R.id.bLimpiar);
         lvLista = findViewById(R.id.lvlista);
 
-        ArrayList<Integer> alnumeros = new ArrayList<>();
+        ArrayList<Float> alnumeros = new ArrayList<>();
 
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getApplicationContext(), android.R.layout.simple_list_item_1,alnumeros);
+        ArrayAdapter<Float> adapter = new ArrayAdapter<Float>(getApplicationContext(), android.R.layout.simple_list_item_1,alnumeros);
 
         bGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String input = etGuardar.getText().toString().trim();
+                float aux = Float.parseFloat(input);
+                float suma=0;
+                int cantidad=0;
+                float promedio=0;
                 if (input.equals(""))
                     Toast.makeText(getApplicationContext(),"Favor de ingresar un número",Toast.LENGTH_LONG).show();
                 else{
-                    int aux = Integer.parseInt(input);
-                    int suma=0;
-                    int cantidad=0;
-                    float promedio=0;
-
+                    alnumeros.add((float) aux);
+                    etGuardar.setText("");
+                    adapter.notifyDataSetChanged();
+                    lvLista.setAdapter(adapter);
                     for (int i = 0; i < alnumeros.size(); i++) {
                         suma = suma + alnumeros.get(i);
                         cantidad=cantidad+1;
                         promedio=suma/cantidad;
-                        }
-
-                    alnumeros.add(aux);
-                    etGuardar.setText("");
-                    adapter.notifyDataSetChanged();
-                    lvLista.setAdapter(adapter);
-                    System.out.println("La cantidad de sueldos es " +cantidad);
-                    System.out.println("La suma de los sueldos es "+suma);
-                    System.out.println("El promedio de los sueldos es " +promedio);
                     }
+                    }
+
+                System.out.println("La cantidad de sueldos es " +cantidad);
+                System.out.println("La suma de los sueldos es "+suma);
+                System.out.println("El promedio de los sueldos es " +promedio);
 
 
                 }
@@ -70,27 +69,11 @@ public class MainActivity extends AppCompatActivity {
         bOrdenar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int temp = 0;
-                for(int i=0; i < alnumeros.size(); i++){
-                    for(int j=1; j < (alnumeros.size()-i); j++){
-                        if(alnumeros.get(j - 1) > alnumeros.get(j)){
-                            //swap elements
-                            temp = alnumeros.get(j - 1);
-                            alnumeros.set(j - 1, alnumeros.get(j));
-                            alnumeros.set(j, temp);
-                        }
-                    }
-                }
-                System.out.println("Array After Bubble Sort");
-                for(int i=0; i < alnumeros.size(); i++){
-                    System.out.print(alnumeros.get(i) + " ");
-                }
-                ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getApplicationContext(), android.R.layout.simple_list_item_1,alnumeros);
-                lvLista.setAdapter(adapter);
+                alnumeros.clear();
             }
         });
 
-        };
+        }
 
 
     }
